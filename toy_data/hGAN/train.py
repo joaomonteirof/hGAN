6fb50885ee729a50f801a88bde1f754332ab2260
joaomonteirof <joaomonteirof@gmail.com>
2018-05-10 15:19:12 -0400

    SGD for MGD

--- toy_data/hGAN/train.py
+++ toy_data/hGAN/train.py
@@ -4,7 +4,7 @@ import os
 import sys
 
 sys.path.insert(0, os.path.realpath(__file__ + ('/..' * 3)))
-print('Running from package root directory {sys.path[0]}')
+print(f'Running from package root directory {sys.path[0]}')
 
 import argparse
 from common.discriminators import Discriminator_toy

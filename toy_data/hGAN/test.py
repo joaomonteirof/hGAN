785cd65a621407331b3cdc2bf20b4a4f74d527b2
IsabelaAlbuquerque <isabelamcalbuquerque@gmail.com>
2018-05-11 19:16:55 -0400

    adding more prints to toy test

--- toy_data/hGAN/test.py
+++ toy_data/hGAN/test.py
@@ -120,6 +120,7 @@ if __name__ == '__main__':
 	parser.add_argument('--n-samples', type=int, default=2500, metavar='N', help='number of samples to  (default: 10000)')
 	parser.add_argument('--toy-dataset', choices=['8gaussians', '25gaussians'], default='8gaussians')
 	parser.add_argument('--no-plots', action='store_true', default=False, help='Disables plot of train/test losses')
+	parser.add_argument('--no-print', action='store_true', default=False, help='Disables print of reached best values of metrics')
 	args = parser.parse_args()
 
 	if args.cp_path is None:
@@ -132,17 +133,26 @@ if __name__ == '__main__':
 
 	history = ckpt['history']
 
-	print('Min FD:', np.min(history['FD']))
-	print('Epoch with min FD:', np.argmin(history['FD']))
-
 	if not args.no_plots:
 		plot_learningcurves(history, 'gen_loss')
 		plot_learningcurves(history, 'disc_loss')
-		# plot_learningcurves(history, 'gen_loss_minibatch')
-		# plot_learningcurves(history, 'disc_loss_minibatch')
 		plot_learningcurves(history, 'FD')
-		plot_learningcurves(history, 'steepest_dir_norm')
-	# plot_learningcurves(history, 'quality_samples')
-	# plot_learningcurves(history, 'quality_modes')
+		#plot_learningcurves(history, 'steepest_dir_norm')
+
+	if not args.no_print:
+		
+		discard = 1
+		discard_ep0 = history['quality_modes'][discard:-1]
+
+		epoch_interest =  np.amax(np.argwhere(discard_ep0 == np.amax((discard_ep0))))
+
+		epoch_interest += discard
+		print('Epoch with Max #high quality modes:', epoch_interest)
+		
+
+		print('Value of FD:', history['FD'][epoch_interest])
+		print('Value of high quality samples (%):', (history['quality_samples'][epoch_interest]/2500.0)*100)
+		print('Value of covered modes:', history['quality_modes'][epoch_interest])
+
 
 	save_samples(generator=generator, cp_name=args.cp_path.split('/')[-1].split('.')[0], save_name=args.cp_path.split('/')[-2].split('.')[0], n_samples=args.n_samples, toy_dataset=args.toy_dataset)

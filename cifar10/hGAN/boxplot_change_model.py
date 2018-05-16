e985021da3a639b1a93de1100bf1d265506202b3
joaomonteirof <joaomonteirof@gmail.com>
2018-05-16 12:15:38 -0400

    substitute box-plot of a single model

--- cifar10/hGAN/boxplot_change_model.py
+++ cifar10/hGAN/boxplot_change_model.py
@@ -4,7 +4,6 @@ import argparse
 
 import os
 import sys
-import glob
 
 sys.path.insert(0, os.path.realpath(__file__ + ('/..' * 3)))
 print(f'Running from package root directory {sys.path[0]}')
@@ -32,13 +31,15 @@ if __name__ == '__main__':
 
 	# Testing settings
 	parser = argparse.ArgumentParser(description='Testing GANs under max hyper volume training')
-	parser.add_argument('--cp-folder', type=str, default=None, metavar='Path', help='Checkpoint/model path')
+	parser.add_argument('--cp-path', type=str, default=None, metavar='Path', help='Checkpoint/model path')
 	parser.add_argument('--ntests', type=int, default=1, metavar='N', help='number of samples to generate (default: 4)')
 	parser.add_argument('--nsamples', type=int, default=2, metavar='Path', help='number of samples per replication')
 	parser.add_argument('--fid-model-path', type=str, default=None, metavar='Path', help='Path to fid model')
 	parser.add_argument('--data-stat-path', type=str, default='../test_data_statistics.p', metavar='Path', help='Path to file containing test data statistics')
 	parser.add_argument('--data-path', type=str, default='../data/', metavar='Path', help='Path to data')
-	parser.add_argument('--out-file', type=str, default='./boxplot_data.p', metavar='Path', help='file for dumping boxplot data')
+	parser.add_argument('--out-file', type=str, default='./boxplot_data_new.p', metavar='Path', help='file for dumping boxplot data')
+	parser.add_argument('--in-file', type=str, default='./boxplot_data.p', metavar='Path', help='file for dumping boxplot data')
+	parser.add_argument('--sub-key', type=str, default=None, metavar='Path', help='key for substitution')
 	parser.add_argument('--model-cifar', choices=['resnet', 'vgg', 'inception'], default='resnet', help='model for FID computation on Cifar. (Default=Resnet)')
 	parser.add_argument('--batch-size', type=int, default=512, metavar='Path', help='batch size')
 	parser.add_argument('--workers', type=int, help='number of data loading workers', default=4)
@@ -61,61 +62,38 @@ if __name__ == '__main__':
 
 	models_dict = {'hyper8': 'HV-8', 'hyper16': 'HV-16', 'hyper24': 'HV-24', 'vanilla8': 'AVG-8', 'vanilla16': 'AVG-16', 'vanilla24': 'AVG-24', 'gman8': 'GMAN-8', 'gman16': 'GMAN-16', 'gman24': 'GMAN-24', 'DCGAN': 'DCGAN', 'WGANGP': 'WGAN-GP'}
 
-	fid_dict = {}
-
 	pfile = open(args.data_stat_path, 'rb')
 	statistics = pickle.load(pfile)
 	pfile.close()
 
+	pfile = open(args.in_file, 'rb')
+	fid_dict = pickle.load(pfile)
+	pfile.close()
+
 	m, C = statistics['m'], statistics['C']
 
-	if args.cp_folder is None:
+	if args.cp_path is None:
 		raise ValueError('There is no checkpoint/model path. Use arg --cp-path to indicate the path!')
 
-	files_list = glob.glob(args.cp_folder + 'G_*.pt')
-	files_list.sort()
+	if args.sub_key is None:
+		raise ValueError('There is no key to substitute. Use arg --sub-key to indicate the key!')
 
-	for file_id in files_list:
+	print(file_name)		
 
-		file_name = file_id.split('/')[-1].split('_')[1]
+	generator = Generator(100, [1024, 512, 256, 128], 3).eval()
+	gen_state = torch.load(args.cp_path, map_location = lambda storage, loc: storage)
+	generator.load_state_dict(gen_state['model_state'])
 
-		print(file_name)		
-
-		key = models_dict[file_name]
-
-		generator = Generator(100, [1024, 512, 256, 128], 3).eval()
-		gen_state = torch.load(file_id, map_location = lambda storage, loc: storage)
-		generator.load_state_dict(gen_state['model_state'])
-
-		if args.cuda:
-			generator = generator.cuda()
-		
-		fid = []
-
-		for i in range(args.ntests):
-			fid.append(compute_fid(generator, fid_model, args.batch_size, args.nsamples, m, C, args.cuda, inception = True if args.model_cifar == 'inception' else False, mnist = False))
-
-		fid_dict[key] = fid
-
-	
-	# Random generator
-	random_generator = Generator(100, [1024, 512, 256, 128], 3).eval()
-	
 	if args.cuda:
-		random_generator = random_generator.cuda()
+		generator = generator.cuda()
 	
-	fid_random = []
+	fid = []
+
 	for i in range(args.ntests):
-		fid_random.append(compute_fid(random_generator, fid_model, args.batch_size, args.nsamples, m, C, args.cuda, inception = True if args.model_cifar == 'inception' else False, mnist = False))
+		fid.append(compute_fid(generator, fid_model, args.batch_size, args.nsamples, m, C, args.cuda, inception = True if args.model_cifar == 'inception' else False, mnist = False))
 
-	
-	# Real data
-	transform = transforms.Compose([transforms.Resize((64, 64), interpolation=Image.BICUBIC), transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-	trainset = datasets.CIFAR10(root=args.data_path, train=True, download=True, transform=transform)
-	train_loader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size, shuffle=True, num_workers=args.workers)
-	fid_real = compute_fid_real_data(train_loader, fid_model, m, C, args.cuda, inception = True if args.model_cifar == 'inception' else False, mnist = False)
+	fid_dict[args.sub_key] = fid
 
-	
 	df = pd.DataFrame(fid_dict)
 	df.head()
 	order_plot = ['DCGAN', 'WGAN-GP', 'AVG-8', 'GMAN-8', 'HV-8', 'AVG-16', 'GMAN-16', 'HV-16', 'AVG-24', 'GMAN-24', 'HV-24']
@@ -125,16 +103,13 @@ if __name__ == '__main__':
 	box.set_ylabel('FID', fontsize = 15)	
 	box.set_yscale('log')
 	plt.grid(True, alpha = 0.3, linestyle = '--')
-	plt.axhline(np.mean(fid_random), color='r', linestyle = 'dashed', linewidth = 1)
-	plt.axhline(fid_real, color='b', linestyle='dashed', linewidth=1)
+	plt.axhline(fid_dict['random'], color='r', linestyle = 'dashed', linewidth = 1)
+	plt.axhline(fid_dict['real'], color='b', linestyle='dashed', linewidth=1)
 	plt.axvline(1.5, color = 'grey', alpha = 0.5, linestyle = 'dashed', linewidth = 1)
 	plt.axvline(4.5, color = 'grey', alpha = 0.5, linestyle = 'dashed', linewidth = 1)
 	plt.axvline(7.5, color = 'grey', alpha = 0.5, linestyle = 'dashed', linewidth = 1)
 	plt.savefig('FID_best_models.pdf')
 	plt.show()
-
-	fid_dict['random'] = np.mean(fid_random)
-	fid_dict['real'] = np.mean(fid_random)
 
 	pfile = open(args.out_file, "wb")
 	pickle.dump(fid_dict, pfile)

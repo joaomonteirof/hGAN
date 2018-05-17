c226eae8fa1bf3f281d40bc0f736fee8dd172cf7
joaomonteirof <joaomonteirof@gmail.com>
2018-05-17 13:19:04 -0400

    minor fixes

--- stacked_mnist/coverage.py
+++ stacked_mnist/coverage.py
@@ -12,6 +12,7 @@ import matplotlib.pyplot as plt
 import torch.utils.data
 
 from common.generators import Generator_stacked_mnist
+from common.models_fid import cnn
 from common.utils import *
 
 if __name__ == '__main__':
@@ -19,6 +20,7 @@ if __name__ == '__main__':
 	# Testing settings
 	parser = argparse.ArgumentParser(description='Testing GANs under max hyper volume training')
 	parser.add_argument('--cp-path', type=str, default=None, metavar='Path', help='Checkpoint/model path')
+	parser.add_argument('--classifier-path', type=str, default=None, metavar='Path', help='Path to pretrained classifier on MNIST')
 	parser.add_argument('--n-tests', type=int, default=4, metavar='N', help='number of samples to generate (default: 4)')
 	parser.add_argument('--no-plots', action='store_true', default=False, help='Disables plot of train/test losses')
 	parser.add_argument('--no-cuda', action='store_true', default=False, help='Disables GPU use')
@@ -27,25 +29,19 @@ if __name__ == '__main__':
 
 	if args.cp_path is None:
 		raise ValueError('There is no checkpoint/model path. Use arg --cp-path to indicate the path!')
+	if args.classifier_path is None:
+		raise ValueError('There is no classifier path. Use arg --classifier-path to indicate the path!')
 
 	model = Generator_mnist()
-
 	ckpt = torch.load(args.cp_path, map_location=lambda storage, loc: storage)
 	model.load_state_dict(ckpt['model_state'])
+	
+	classifier = cnn().eval()
+	ckpt = torch.load(args.classifier_path, map_location=lambda storage, loc: storage)
+	classifier.load_state_dict(ckpt['model_state'])
 
 	if args.cuda:
 		model = model.cuda()
+		classifier = classifier.cuda()
 
 	print('Cuda Mode is: {}'.format(args.cuda))
-
-	history = ckpt['history']
-
-	if not args.no_plots:
-		plot_learningcurves(history, 'gen_loss')
-		plot_learningcurves(history, 'disc_loss')
-		plot_learningcurves(history, 'gen_loss_minibatch')
-		plot_learningcurves(history, 'disc_loss_minibatch')
-		plot_learningcurves(history, 'steepest_dir_norm')
-
-	test_model(model=model, n_tests=args.n_tests, cuda_mode=args.cuda)
-	save_samples(generator=model, cp_name=args.cp_path.split('/')[-1].split('.')[0], prefix='stacked_mnist', fig_size=(10, 10), nc=3, im_size=28, cuda_mode=args.cuda)

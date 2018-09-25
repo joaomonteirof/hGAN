67616789cfd0d845a3b919cd39417cb0f1eafc2e
IsabelaAlbuquerque <isabelamcalbuquerque@gmail.com>
2018-09-25 11:51:06 -0400

    updating tiny-imagenet stats

--- tiny_imagenet/train.py
+++ tiny_imagenet/train.py
@@ -44,7 +44,7 @@ torch.manual_seed(args.seed)
 if args.cuda:
 	torch.cuda.manual_seed(args.seed)
 
-transform = transforms.Compose([transforms.Resize((64, 64), interpolation=Image.BICUBIC), transforms.RandomHorizontalFlip(), transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+transform = transforms.Compose([transforms.Resize((64, 64), interpolation=Image.BICUBIC), transforms.RandomHorizontalFlip(), transforms.ToTensor(), transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))])
 
 data_set = datasets.ImageFolder(args.data_path, transform=transform)
 

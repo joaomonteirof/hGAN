fd636f9c17dbf50bc37acaa875531c5fc62bdc55
IsabelaAlbuquerque <isabelamcalbuquerque@gmail.com>
2018-10-19 16:25:37 -0400

    removing batchnorm from wgan-gp disc

--- cifar10_64/wGAN-gp/train.py
+++ cifar10_64/wGAN-gp/train.py
@@ -54,7 +54,7 @@ trainset = datasets.CIFAR10(root=args.data_path, train=True, download=True, tran
 train_loader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size, num_workers=args.workers)
 
 generator = Generator(100, [1024, 512, 256, 128], 3).train()
-disc = Discriminator_wgan(3, [128, 256, 512, 1024], 1, optim.Adam, args.lr, (args.beta1, args.beta2), batch_norm=True).train()
+disc = Discriminator(3, [128, 256, 512, 1024], 1, optim.Adam, args.lr, (args.beta1, args.beta2), batch_norm=False).train()
 fid_model = ResNet18().eval()
 mod_state = torch.load(args.fid_model_path, map_location=lambda storage, loc: storage)
 fid_model.load_state_dict(mod_state['model_state'])
